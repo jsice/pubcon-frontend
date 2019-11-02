e9040dcedd4c69a48356e1bc8850cef149986fa3
Thanadon <thanadon.p@ku.th>
2019-11-02 12:17:43 +0700

    Login with google prototype

---
+++ src/firebaseConfig.js
@@ -0,0 +1,12 @@
+const config = {
+    apiKey: "AIzaSyA0DZksXhVkAbfME-4xgj21i808FjUWtks",
+    authDomain: "pubcon-da166.firebaseapp.com",
+    databaseURL: "https://pubcon-da166.firebaseio.com",
+    projectId: "pubcon-da166",
+    storageBucket: "pubcon-da166.appspot.com",
+    messagingSenderId: "504664700327",
+    appId: "1:504664700327:web:5e06c75c4aa0920260f282",
+    measurementId: "G-3VNZQWCET2"
+};
+
+export default config;
