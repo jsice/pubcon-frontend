5313aaa6723e3b44103dd3eefc17d88a1015120d
Ton Vudhidej <vudhidej.d@gmail.com>
2019-11-02 20:30:22 +0700

    create problems route

---
+++ src/containers/ProblemsPage/index.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const ProblemsPage = () => {
+  return <p>Problems</p>
+}
+
+export default ProblemsPage
--- src/containers/Router/index.js
+++ src/containers/Router/index.js
@@ -1,10 +1,12 @@
 import React from "react"
 import { Switch, Route } from "react-router-dom"
 import HomePage from "../HomePage"
+import ProblemsPage from "../ProblemsPage"
 
 const Routes = () => (
   <Switch>
     <Route exact path="/" component={HomePage} />
+    <Route exact path="/problems" component={ProblemsPage} />
   </Switch>
 )
 
